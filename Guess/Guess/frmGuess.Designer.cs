e81d214ee5c72b2ea352456c228192e5417326f5
tiago-gmfrr <tiago.gmfrr@eduge.ch>
2019-02-15 11:34:51 +0100

    Premiere version travail

---
+++ Guess/Guess/frmGuess.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Guess
+{
+    partial class frmGuess
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.btnNon = new System.Windows.Forms.Button();
+            this.btnOui = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.AutoSize = true;
+            this.lblQuestion.Location = new System.Drawing.Point(318, 108);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(35, 13);
+            this.lblQuestion.TabIndex = 0;
+            this.lblQuestion.Text = "label1";
+            // 
+            // btnNon
+            // 
+            this.btnNon.Location = new System.Drawing.Point(128, 310);
+            this.btnNon.Name = "btnNon";
+            this.btnNon.Size = new System.Drawing.Size(115, 60);
+            this.btnNon.TabIndex = 1;
+            this.btnNon.Text = "button1";
+            this.btnNon.UseVisualStyleBackColor = true;
+            this.btnNon.Click += new System.EventHandler(this.btnNon_Click);
+            // 
+            // btnOui
+            // 
+            this.btnOui.Location = new System.Drawing.Point(455, 310);
+            this.btnOui.Name = "btnOui";
+            this.btnOui.Size = new System.Drawing.Size(115, 60);
+            this.btnOui.TabIndex = 2;
+            this.btnOui.Text = "button2";
+            this.btnOui.UseVisualStyleBackColor = true;
+            this.btnOui.Click += new System.EventHandler(this.btnOui_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnOui);
+            this.Controls.Add(this.btnNon);
+            this.Controls.Add(this.lblQuestion);
+            this.Name = "Form1";
+            this.Text = "Guess";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.Button btnNon;
+        private System.Windows.Forms.Button btnOui;
+    }
+}
+
---
+++ Guess/Guess/frmGuess.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Guess
+{
+    public partial class frmGuess : Form
+    {
+        bool reponse = false;
+        IDictionary<string, string> Kaaris = new Dictionary<string, string>();
+        IDictionary<string, string> SnoopDogg = new Dictionary<string, string>();        
+        IDictionary<string, string> ClaudeFrançois = new Dictionary<string, string>();
+        IDictionary<string, string> EmmaWatson = new Dictionary<string, string>();        
+        IDictionary<string, string> PassePartout = new Dictionary<string, string>();
+        IDictionary<string, string> DenisBrogniart = new Dictionary<string, string>();
+        IDictionary<string, string> MaitreGims = new Dictionary<string, string>();
+        IDictionary<string, string> EmiliaClark = new Dictionary<string, string>();
+        IDictionary<string, string> MadsMikkelsen = new Dictionary<string, string>();
+        IDictionary<string, string> ScarlettJohansson = new Dictionary<string, string>();
+        public frmGuess()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnOui_Click(object sender, EventArgs e)
+        {
+            reponse = true;
+        }
+
+        private void btnNon_Click(object sender, EventArgs e)
+        {
+            reponse = false;
+
+            
+        }
+
+        private void NewGame()
+        {
+            string[] lignes;
+            var resources = Properties.Resources.Personnages__Criteres;
+
+            lignes = resources.Split(new[] { Environment.NewLine },
+    StringSplitOptions.None);
+
+            foreach (string line in lignes)
+            {
+                string[] tmp = line.Split('//');
+                string nom = tmp[0];
+
+                foreach (string caracteristique in tmp)
+                {
+
+                    switch (nom)
+                    {
+                        case "Kaaris":
+
+                           // Kararis.Add(swdshak,caracteristique);
+                            break;
+                    }
+                
+            }
+            }
+        }
+    }
+}
